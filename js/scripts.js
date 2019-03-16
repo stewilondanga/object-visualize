5bd135ae4c384291483758c8549760128c9ec67c
Stewart ilondanga <stilondanga@gmail.com>
2019-03-16 15:50:18 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-var __assign = this && this.__assign || Object.assign || function (t) {
+/*var __assign = this && this.__assign || Object.assign || function (t) {
   for (var s, i = 1, n = arguments.length; i < n; i++) {if (window.CP.shouldStopExecution(0)) break;
     s = arguments[i];
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
